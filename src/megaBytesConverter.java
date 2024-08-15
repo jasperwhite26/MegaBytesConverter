e94d7cd03287582c3f4d464e4d47d99ebc7748df
jasper <whiteana001@warren.k12.in.us>
2024-08-15 13:21:42 -0400

    initial commit

--- src/megaBytesConverter.java
+++ src/megaBytesConverter.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class megaBytesConverter {
+public class megaBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKilobytes(2500);
+        printMegaBytesAndKilobytes(-1024);
+        printMegaBytesAndKilobytes(5000);
+    }
+
+    public static void printMegaBytesAndKilobytes(int kilobytes){
+        if (kilobytes > 0) {
+            int mega = kilobytes / 1024;
+            int kb = kilobytes % 1024;
+            System.out.println(kilobytes + " KB = " + mega + " MB and " + kb + " KB");
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
 }
